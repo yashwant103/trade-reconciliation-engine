1fe16760010cc865220cb237ffafbae26345be56
yashwant103 <yashwantjadhao9@gmail.com>
2026-09-12 17:21:32 +0530

    Phase 0: Initial project structure and mock data generator

---
+++ src/main/java/com/github/yashwant3/reconengine/checkpoint/Main.java
@@ -0,0 +1,7 @@
+package com.github.yashwant3.reconengine.checkpoint;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Trade Reconiliation Engine initialized on Java "+ System.getProperty("java.version"));
+    }
+}
